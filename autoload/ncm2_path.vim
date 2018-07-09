90591f31424df020e355aa60bd564399a9544d02
roxma <roxma@qq.com>
2018-07-09 18:51:27 +0800

    split path completion source. close #2
    - bufpath
    - cwdpath
    - rootpath

--- autoload/ncm2_path.vim
+++ autoload/ncm2_path.vim
@@ -5,34 +5,79 @@ let s:loaded = 1
 
 let g:ncm2_path#proc = yarp#py3('ncm2_path')
 
-let g:ncm2_path#source = get(g:, 'ncm2_path#source', {
-            \ 'name': 'path',
+let g:ncm2_path#bufpath_source = get(g:, 'ncm2_path#bufpath_source', {
+            \ 'name': 'bufpath',
             \ 'priority': 6,
             \ 'mark': '/',
             \ 'word_pattern': '([^\W]|[-.~%$])+',
             \ 'complete_pattern': [
             \       '(\.[/\\]+|[a-zA-Z]:\\+|~\/+)',
             \       '([^\W]|[-.~%$]|[/\\])+[/\\]+'],
-            \ 'on_complete': 'ncm2_path#on_complete',
+            \ 'on_complete': 'ncm2_path#on_complete_bufpath',
             \ 'on_warmup': 'ncm2_path#on_warmup',
             \ })
 
-let g:ncm2_path#source = extend(g:ncm2_path#source,
-            \ get(g:, 'ncm2_path#source_override', {}),
+let g:ncm2_path#bufpath_source = extend(g:ncm2_path#bufpath_source,
+            \ get(g:, 'ncm2_path#bufpath_source_override', {}),
+            \ 'force')
+
+let g:ncm2_path#cwdpath_source = get(g:, 'ncm2_path#cwdpath_source', {
+            \ 'name': 'cwdpath',
+            \ 'priority': 5,
+            \ 'mark': '/',
+            \ 'word_pattern': '([^\W]|[-.~%$])+',
+            \ 'complete_pattern': [
+            \       '(\.[/\\]+|[a-zA-Z]:\\+|~\/+)',
+            \       '([^\W]|[-.~%$]|[/\\])+[/\\]+'],
+            \ 'on_complete': 'ncm2_path#on_complete_cwdpath',
+            \ 'on_warmup': 'ncm2_path#on_warmup',
+            \ })
+
+let g:ncm2_path#cwdpath_source = extend(g:ncm2_path#cwdpath_source,
+            \ get(g:, 'ncm2_path#cwdpath_source_override', {}),
+            \ 'force')
+
+let g:ncm2_path#rootpath_source = get(g:, 'ncm2_path#rootpath_source', {
+            \ 'name': 'rootpath',
+            \ 'priority': 5,
+            \ 'mark': '/',
+            \ 'word_pattern': '([^\W]|[-.~%$])+',
+            \ 'complete_pattern': [
+            \       '(\.[/\\]+|[a-zA-Z]:\\+|~\/+)',
+            \       '([^\W]|[-.~%$]|[/\\])+[/\\]+'],
+            \ 'on_complete': 'ncm2_path#on_complete_rootpath',
+            \ 'on_warmup': 'ncm2_path#on_warmup',
+            \ })
+
+let g:ncm2_path#rootpath_source = extend(g:ncm2_path#rootpath_source,
+            \ get(g:, 'ncm2_path#rootpath_source_override', {}),
             \ 'force')
 
 let g:ncm2_path#path_pattern = get(g:
             \ , 'ncm2_path#path_pattern', '(([^\W]|[-.~%$]|[/\\])+)')
 
 func! ncm2_path#init()
-    call ncm2#register_source(g:ncm2_path#source)
+    call ncm2#register_source(g:ncm2_path#bufpath_source)
+    call ncm2#register_source(g:ncm2_path#cwdpath_source)
+    call ncm2#register_source(g:ncm2_path#rootpath_source)
 endfunc
 
 func! ncm2_path#on_warmup(ctx)
     call g:ncm2_path#proc.jobstart()
 endfunc
 
-func! ncm2_path#on_complete(ctx)
-    call g:ncm2_path#proc.try_notify('on_complete', a:ctx, g:ncm2_path#path_pattern)
+func! ncm2_path#on_complete_bufpath(ctx)
+    if empty(a:ctx.filepath)
+        return
+    endif
+    call g:ncm2_path#proc.try_notify('on_complete_bufpath', a:ctx, g:ncm2_path#path_pattern)
+endfunc
+
+func! ncm2_path#on_complete_cwdpath(ctx)
+    call g:ncm2_path#proc.try_notify('on_complete_cwdpath', a:ctx, g:ncm2_path#path_pattern, getcwd())
+endfunc
+
+func! ncm2_path#on_complete_rootpath(ctx)
+    call g:ncm2_path#proc.try_notify('on_complete_rootpath', a:ctx, g:ncm2_path#path_pattern)
 endfunc
 
--- pythonx/ncm2_path.py
+++ pythonx/ncm2_path.py
@@ -10,7 +10,7 @@ logger = getLogger(__name__)
 
 class Source(Ncm2Source):
 
-    def on_complete(self, ctx, path_pattern):
+    def on_complete_bufpath(self, ctx, path_pattern):
         typed = ctx['typed']
         filepath = ctx['filepath']
         startccol = ctx['startccol']
@@ -27,56 +27,150 @@ class Source(Ncm2Source):
 
         logger.debug('dir: %s', dr)
 
-        base_dirs = []
-        if filepath != "":
-            curdir = path.dirname(filepath)
-            base_dirs.append(('buf', curdir), )
-
-        # full path of current file, current working dir
-        cwd = self.nvim.call('getcwd')
-        base_dirs.append(('cwd', cwd), )
-
-        if path_keywrod and path_keywrod[0] != ".":
-            base_dirs.append(('root', "/"))
+        label = 'buf'
+        base_dir = path.dirname(filepath)
 
         matcher = self.matcher_get(ctx['matcher'])
 
-        seen = set()
         matches = []
-        for label, base_dir in base_dirs:
-            joined_dir = path.join(base_dir, dr.strip('/'))
-            logger.debug('searching dir: %s', joined_dir)
-            try:
-                names = listdir(joined_dir)
-                names.sort(key=lambda name: name.lower())
-                logger.debug('search result: %s', names)
-                for name in names:
-                    p = path.join(joined_dir, name)
-                    if p in seen:
-                        continue
-                    seen.add(p)
-                    word = path.basename(p)
-                    menu = '~' + label
-                    if expanded:
-                        menu += '~ ' + p
 
-                    m = dict(word=word, menu='~' + label, dup=1)
-                    m = self.match_formalize(ctx, m)
-                    if matcher(base, m):
-                        matches.append(m)
-            except Exception as ex:
-                logger.exception( 'failed searching dir [%s]', joined_dir)
-                continue
+        joined_dir = path.join(base_dir, dr.strip('/'))
+        logger.debug('searching dir: %s', joined_dir)
+        try:
+            names = listdir(joined_dir)
+            names.sort(key=lambda name: name.lower())
+            logger.debug('search result: %s', names)
+            for name in names:
+                p = path.join(joined_dir, name)
+                word = path.basename(p)
+                menu = '~' + label
+                if expanded:
+                    menu += '~ ' + p
+
+                m = dict(word=word, menu='~' + label)
+                m = self.match_formalize(ctx, m)
+                if matcher(base, m):
+                    matches.append(m)
+        except Exception as ex:
+            logger.exception('failed searching dir [%s]', joined_dir)
 
         refresh = 0
 
-        if len(matches) > 1024:
+        if len(matches) > 100:
             refresh = 1
-            matches = matches[0:1024]
+            matches = matches[0:100]
+
+        self.complete(ctx, startccol, matches, refresh)
+
+    def on_complete_cwdpath(self, ctx, path_pattern, cwd):
+        typed = ctx['typed']
+        startccol = ctx['startccol']
+        base = ctx['base']
+
+        path_keywrod = re.search(path_pattern + '$', typed).group(0)
+
+        dr = path.expandvars(path_keywrod)
+        dr = path.expanduser(dr)
+        expanded = False
+        if dr != path_keywrod:
+            expanded = True
+        dr = path.dirname(dr)
+
+        logger.debug('dir: %s', dr)
+
+        label = 'cwd'
+        base_dir = cwd
+
+        matcher = self.matcher_get(ctx['matcher'])
+
+        matches = []
+
+        joined_dir = path.join(base_dir, dr.strip('/'))
+        logger.debug('searching dir: %s', joined_dir)
+        try:
+            names = listdir(joined_dir)
+            names.sort(key=lambda name: name.lower())
+            logger.debug('search result: %s', names)
+            for name in names:
+                p = path.join(joined_dir, name)
+                word = path.basename(p)
+                menu = '~' + label
+                if expanded:
+                    menu += '~ ' + p
+
+                m = dict(word=word, menu='~' + label)
+                m = self.match_formalize(ctx, m)
+                if matcher(base, m):
+                    matches.append(m)
+        except Exception as ex:
+            logger.exception('failed searching dir [%s]', joined_dir)
+
+        refresh = 0
+
+        if len(matches) > 100:
+            refresh = 1
+            matches = matches[0:100]
+
+        self.complete(ctx, startccol, matches, refresh)
+
+    def on_complete_rootpath(self, ctx, path_pattern):
+        typed = ctx['typed']
+        filepath = ctx['filepath']
+        startccol = ctx['startccol']
+        base = ctx['base']
+
+        path_keywrod = re.search(path_pattern + '$', typed).group(0)
+
+        dr = path.expandvars(path_keywrod)
+        dr = path.expanduser(dr)
+        expanded = False
+        if dr != path_keywrod:
+            expanded = True
+        dr = path.dirname(dr)
+
+        logger.debug('dir: %s', dr)
+
+        if not path_keywrod.startswith('/'):
+            return
+
+        label = 'root'
+        base_dir = '/'
+
+        matcher = self.matcher_get(ctx['matcher'])
+
+        matches = []
+
+        joined_dir = path.join(base_dir, dr)
+        logger.debug('searching dir: %s', joined_dir)
+        try:
+            names = listdir(joined_dir)
+            names.sort(key=lambda name: name.lower())
+            logger.debug('search result: %s', names)
+            for name in names:
+                p = path.join(joined_dir, name)
+                word = path.basename(p)
+                menu = '~' + label
+                if expanded:
+                    menu += '~ ' + p
+
+                m = dict(word=word, menu='~' + label)
+                m = self.match_formalize(ctx, m)
+                if matcher(base, m):
+                    matches.append(m)
+        except Exception as ex:
+            logger.exception('failed searching dir [%s]', joined_dir)
+
+        refresh = 0
+
+        if len(matches) > 100:
+            refresh = 1
+            matches = matches[0:100]
 
         self.complete(ctx, startccol, matches, refresh)
 
 
 source = Source(vim)
 
-on_complete = source.on_complete
+on_complete_bufpath = source.on_complete_bufpath
+on_complete_cwdpath = source.on_complete_cwdpath
+on_complete_rootpath = source.on_complete_rootpath
